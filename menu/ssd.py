97618c623d39816ed4bfd0736213fd9c3ec52d44
wj9183 <wj9183@daum.net>
2021-04-29 12:02:37 +0900

    edit

--- menu/ssd.py
+++ menu/ssd.py
@@ -152,12 +152,23 @@ def ssd_video():
     video_file_origin = open('menu/test_video/ssd_test_video_1.mp4', 'rb').read()     #비디오 파일 읽어와라. 'rb'(어떤 용도로 읽어올 건지) 안써주면 안됨.
     st.video(video_file_origin)
     
-    
-    st.header("원본")
-    video_file_origin = open('test_data/videos/origin1.mp4', 'rb').read()     #비디오 파일 읽어와라. 'rb'(어떤 용도로 읽어올 건지) 안써주면 안됨.
-    st.video(video_file_origin)
+    blank = """ """
+    st.write(blank)
+    st.write(blank)
+    st.write(blank)
+    st.write(blank)
+    st.write(blank)
+    st.write(blank)
+    st.write(blank)
 
-    if st.button('변환'):
+    menu = ['원본 영상', 'Object Detection']
+    select = st.radio("메뉴를 골라주세요.", menu)
+
+    if select == '원본 영상':
+      video_file_origin = open('test_data/videos/origin1.mp4', 'rb').read()     #비디오 파일 읽어와라. 'rb'(어떤 용도로 읽어올 건지) 안써주면 안됨.
+      st.video(video_file_origin)
+
+    elif select == 'Object Detection':
         st.header("Object Detection")
         video_file_convert = open('test_data/videos/convert1.mp4', 'rb').read()
         st.video(video_file_convert)
