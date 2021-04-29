9bdaab4097fd3db88d3e407ce35a8ae51e34f360
wj9183 <wj9183@daum.net>
2021-04-29 12:59:12 +0900

    semantic segmentation edit

--- menu/ssd.py
+++ menu/ssd.py
@@ -165,6 +165,7 @@ def ssd_video():
     select = st.radio("메뉴를 골라주세요.", menu)
 
     if select == '원본 영상':
+      st.header("원본 영상")
       video_file_origin = open('test_data/videos/origin1.mp4', 'rb').read()     #비디오 파일 읽어와라. 'rb'(어떤 용도로 읽어올 건지) 안써주면 안됨.
       st.video(video_file_origin)
 
