96d7626df5f68614c8fbe0b942940d71d6ae4232
wj9183 <wj9183@daum.net>
2021-04-30 01:12:03 +0900

    remains about segmentation

--- app.py
+++ app.py
@@ -4,7 +4,32 @@ from menu.ssd import ssd_video, about_ssd
 from menu.yolo import yolo_video, about_yolo
 from menu.semantic_segmentation import semantic_segmentation_image, semantic_segmentation_video, about_semantic_segmentation
 
+# import base64
+
+# main_bg = "script/001.png"
+# main_bg_ext = "jpg"
+
+# side_bg = "script/001.png"
+# side_bg_ext = "jpg"
+
+# st.markdown(
+#     f"""
+#     <style>
+#     .reportview-container {{
+#         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+#     }}
+#    .sidebar .sidebar-content {{
+#         background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
 def main():
+
+
+    
     menu = ['Object Detection', 'SSD', 'YOLO', 'Semantic Segmentation']
     choice = st.sidebar.selectbox("메뉴", menu)
     
